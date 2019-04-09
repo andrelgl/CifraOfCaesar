f18f276a4ea22451a51026c62455b67aa6de8862
André Luiz <maki.karasu@gmail.com>
2019-04-09 17:50:10 -0300

    decrypt implementate

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 // Caesar struct
 type Caesar struct {
@@ -8,17 +11,49 @@ type Caesar struct {
 }
 
 // NewCaesar pao com ovo
-func NewCaesar(key int) Caesar {
+func NewCaesar(key int) map[string]string {
 	matrix := make(map[string]string)
 	words := "abcdefghijklmnopqrstuvwxyz"
 	i := words[len(words)-(len(words)-(key+1)):]
 	w := words[:len(words)-(len(words)-(key+1))]
 	cypher := i + w
-	return matrix[words] = cypher
+	for i := 0; i < len(words); i++ {
+		matrix[string(words[i])] = string(cypher[i])
+	}
+	return matrix
+}
+
+// Encryption for simple words
+func Encryption(key int, word string) string {
+	matrix := NewCaesar(key)
+	output := ""
+	for i := 0; i < len(word); i++ {
+		output = output + matrix[string(word[i])]
+	}
+	return output
+}
+
+// Decrypt for simples words
+func Decrypt(key int, word string) string {
+	matrixO := NewCaesar(key)
+	matrix := map[string]string{}
+	keys := []string{}
+	output := ""
+
+	for k, v := range matrixO {
+		matrix[v] = k
+		keys = append(keys, v)
+	}
+	for _, k := range keys {
+		output = output + string(matrix[k][strings.Index(k, string(word))])
+	}
+	return output
 }
 
 func main() {
-	a := NewCaesar(6)
-	log.Println(a.Matrix)
+	a := Encryption(6, "batata")
+	b := Decrypt(6, "ihahah")
+	log.Println(a)
+	log.Println(b)
 
 }
